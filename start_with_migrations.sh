5783d0661c343f2f48ec0861cba3e82d067a0f3c
majudhu <majudhu.sup@gmail.com>
2023-04-06 08:41:42 +0500

    add uploads setup

--- start_with_migrations.sh
+++ start_with_migrations.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if [[ ! -d /data/uploads ]]
+  then
+    mkdir /data/uploads
+fi
+
+ln -s /data/uploads public/uploads
+
 set -ex
 pnpm exec prisma migrate deploy
 pnpm exec prisma db seed

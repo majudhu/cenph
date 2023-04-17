a7a538e1779b5d812e794f8ab8c0381712a0a39f
majudhu <majudhu.sup@gmail.com>
2023-04-17 23:50:56 +0500

    if on fly, use internal network and refactors better error messages and show link only if uploaded file exists

--- cenph-reminder/src/main.rs
+++ cenph-reminder/src/main.rs
@@ -11,24 +11,50 @@ type AsyncResult<T> = Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 #[tokio::main]
 async fn main() -> AsyncResult<()> {
-    let api_url = &env::var("API_URL")?;
-    let chat_id = ChatId((env::var("TG_CHAT_ID")?).parse()?);
+    let ref web_url = env::var("WEB_URL").expect("Env WEB_URL is required");
+    // use fly internal network if on fly
+    let internal_url = match env::var("FLY_APP_NAME") {
+        Ok(app) => Some(match env::var("PORT") {
+            Ok(port) => format!("http://{}.internal:{}", app, port),
+            _ => format!("http://{}.internal", app),
+        }),
+        _ => None,
+    };
+    let ref api_url = format!(
+        "{}/prescriptions/expiring",
+        internal_url.as_ref().unwrap_or(web_url)
+    );
+    let chat_id = ChatId(
+        (env::var("TG_CHAT_ID").expect("Env TG_CHAT_ID is required"))
+            .parse()
+            .expect("Env TG_CHAT_ID should be a number"),
+    );
 
     loop {
-        let prescriptions: Vec<Prescription> = api_req("GET", Body::empty()).await?;
+        let prescriptions: Vec<Prescription> = api_req(api_url, "GET", Body::empty()).await?;
         println!("{} expiring", prescriptions.len());
 
         let notified_ids: Vec<u64> =
             join_all(prescriptions.into_iter().map(|prescription| async move {
-                let message = format!(
-                    "Prescription expiring for “{}” on {}\n{}/prescriptions/{}\n{}/uploads/{}",
-                    prescription.customer.name,
-                    prescription.renewal_date.format("%Y-%m-%d"),
-                    api_url,
-                    prescription.id,
-                    api_url,
-                    prescription.prescription
-                );
+                let message = if prescription.prescription.is_empty() {
+                    format!(
+                        "Prescription expiring for “{}” on {}\n{}/prescriptions/{}",
+                        prescription.customer.name,
+                        prescription.renewal_date.format("%Y-%m-%d"),
+                        web_url,
+                        prescription.id,
+                    )
+                } else {
+                    format!(
+                        "Prescription expiring for “{}” on {}\n{}/prescriptions/{}\n{}/uploads/{}",
+                        prescription.customer.name,
+                        prescription.renewal_date.format("%Y-%m-%d"),
+                        web_url,
+                        prescription.id,
+                        web_url,
+                        prescription.prescription
+                    )
+                };
                 if Bot::from_env().send_message(chat_id, message).await.is_ok() {
                     Option::Some(prescription.id)
                 } else {
@@ -43,10 +69,13 @@ async fn main() -> AsyncResult<()> {
 
         println!("{} notified: {:?}", notified_ids.len(), notified_ids);
 
-        let mark_notified_status =
-            api_req_blank("POST", Body::from(serde_json::to_string(&notified_ids)?))
-                .await?
-                .status();
+        let mark_notified_status = api_req_blank(
+            api_url,
+            "POST",
+            Body::from(serde_json::to_string(&notified_ids)?),
+        )
+        .await?
+        .status();
         if mark_notified_status.is_success() {
             println!("marked notified");
         } else {
@@ -57,22 +86,21 @@ async fn main() -> AsyncResult<()> {
     }
 }
 
-async fn api_req_blank(method: &str, body: Body) -> AsyncResult<Response<Body>> {
+async fn api_req_blank(url: &str, method: &str, body: Body) -> AsyncResult<Response<Body>> {
     let client = Client::builder().build(HttpsConnector::new());
-    let api_url = &format!("{}/prescriptions/expiring", &env::var("API_URL")?);
     let request = Request::builder()
-        .uri(api_url)
+        .uri(url)
         .method(method)
         .header("authorization", &env::var("API_KEY")?)
         .body(body)?;
     Ok(client.request(request).await?)
 }
 
-async fn api_req<T>(method: &str, body: Body) -> AsyncResult<T>
+async fn api_req<T>(url: &str, method: &str, body: Body) -> AsyncResult<T>
 where
     T: DeserializeOwned,
 {
-    let response = api_req_blank(method, body).await?;
+    let response = api_req_blank(url, method, body).await?;
     let body = body::aggregate(response).await?;
     Ok(serde_json::from_reader(body.reader())?)
 }
